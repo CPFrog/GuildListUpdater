39670a22de5ab902b7150b47eb78e13b4bb9a524
강개굴 <kokk012@naver.com>
2022-01-04 13:52:32 +0900

    v1.1 update - additionally supports edge - reduced waiting time

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import Qt
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import chromedriver_autoinstaller
-from PyQt5 import QtCore
+import edgedriver_autoinstaller
 
 guild_url = "https://loawa.com/guild/"
 url = "https://loawa.com/char/"
@@ -17,7 +17,7 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('로스트아크 길드원 목록 갱신')
+        self.setWindowTitle('로스트아크 길드원 목록 갱신 v1.1 by CP개구링')
         grid = QGridLayout()
         self.setLayout(grid)
 
@@ -29,13 +29,8 @@ class MyApp(QWidget):
 
         search_btn = QPushButton('검색', self)
         search_btn.clicked.connect(self.button_event)
-        grid.addWidget(search_btn, 2, 1)
+        grid.addWidget(search_btn, 1, 1)
 
-        notice_label = QLabel('현재 검사중: ')
-        grid.addWidget(notice_label, 1, 0)
-
-        self.cur_name = QLabel('')
-        grid.addWidget(self.cur_name, 1, 1)
         self.setGeometry(200, 200, 300, 100)
         self.show()
 
@@ -53,37 +48,46 @@ class MyApp(QWidget):
         elif e.key() == Qt.Key_Escape:
             self.close()
 
-    def update_label(self):
-        self.cur_name.repaint()
-
     def closeEvent(self, e):
         QMessageBox.information(self, '프로그램 종료 알림', '프로그램이 종료됩니다.')
 
     def list_update(self, guild_name):
-        options = webdriver.ChromeOptions()  # 옵션 생성
-        options.add_argument("--headless")  # 창 숨기는 옵션 추가
-        global chrome_ver
-        chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]  # 크롬 드라이버 버전 확인
-
+        global driver_ver
         try:
-            driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver', options=options)
+            driver_ver = edgedriver_autoinstaller.get_edge_version().split('.')[0]  # 크롬 드라이버 버전 확인
+            browser = "edge"
         except:
-            chromedriver_autoinstaller.install(True)
-            driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver', options=options)
+            driver_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+            browser = "chrome"
 
-        driver.implicitly_wait(10)
+        if browser == "edge":
+            options = webdriver.EdgeOptions()  # 옵션 생성
+            options.add_argument("--headless")  # 창 숨기는 옵션 추가
+            try:
+                driver = webdriver.Edge(f'./{driver_ver}/edgedriver', options=options)
+            except:
+                edgedriver_autoinstaller.install(True)
+                driver = webdriver.Edge(f'./{driver_ver}/edgedriver', options=options)
+
+        elif browser == "chrome":
+            options = webdriver.ChromeOptions()  # 옵션 생성
+            options.add_argument("--headless")  # 창 숨기는 옵션 추가
+            try:
+                driver = webdriver.Chrome(f'./{driver_ver}/chromedriver', options=options)
+            except:
+                chromedriver_autoinstaller.install(True)
+                driver = webdriver.Chrome(f'./{driver_ver}/chromedriver', options=options)
+
+        driver.implicitly_wait(3)
 
         driver.get(guild_url + guild_name)
         guild_soup = BeautifulSoup(driver.page_source, 'html.parser')
-        # print(guild_soup)
         member_list = guild_soup.find_all('table', {'class': 'tfs13'})
 
         for iter in member_list:
             cname = iter.find('span', {'class': 'text-theme-0 tfs13'}).text.strip()
-            self.cur_name.setText(cname)
-            self.cur_name.repaint()
             driver.get(url + cname)
-            time.sleep(2)
+            # time.sleep(1)
 
         driver.quit()
 
@@ -91,14 +95,4 @@ class MyApp(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
-
-
-    def refresh():
-        ex.update_label
-
-
-    timer = QtCore.QTimer()
-    timer.timeout.connect(refresh)
-    timer.start(2000)
-
     sys.exit(app.exec_())
